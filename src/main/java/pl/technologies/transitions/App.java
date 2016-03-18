049114bcadd260522383ecb1e7182677a3f43c56
gc <gc>
2016-03-18 09:07:38 +0100

    commit numero uno

---
+++ src/main/java/pl/technologies/transitions/App.java
@@ -0,0 +1,13 @@
+package pl.technologies.transitions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
